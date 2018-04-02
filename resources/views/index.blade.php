3ac117d651f02bcdbd468a6126e871dd6f490965
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2018-04-02 18:07:56 +0800

    Show three products

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,84 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>this is my shop</h1>
+  <div class="columns">
+    <div class="column">
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">
+            Product title
+          </p>
+          <a href="#" class="card-header-icon" aria-label="more options">
+            <span class="icon">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </a>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+            <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+            <br>
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+        <footer class="card-footer">
+          <p class="card-footer-item">1,000円</p>
+          <a href="#" class="card-footer-item">Add to Cart</a>
+        </footer>
+      </div>
+    </div>
+    <div class="column">
+        <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">
+            Product title
+          </p>
+          <a href="#" class="card-header-icon" aria-label="more options">
+            <span class="icon">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </a>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+            <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+            <br>
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+        <footer class="card-footer">
+          <p class="card-footer-item">1,000円</p>
+          <a href="#" class="card-footer-item">Add to Cart</a>
+        </footer>
+      </div>
+    </div>
+    <div class="column">
+        <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">
+            Product title
+          </p>
+          <a href="#" class="card-header-icon" aria-label="more options">
+            <span class="icon">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </a>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+            <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+            <br>
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+        <footer class="card-footer">
+          <p class="card-footer-item">1,000円</p>
+          <a href="#" class="card-footer-item">Add to Cart</a>
+        </footer>
+      </div>
+    </div>
+  </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,10 @@
   <body>
     <div id="app">
       @include('partials.header')
-      @yield('content')
+      
+      <div class="container">
+          @yield('content')
+      </div>
     </div>
   </body>
 </html>
